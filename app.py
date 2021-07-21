ec2d4f20fba3b6e87fdf04b2b6cb39de3c97e34f
Sam <sathiyamurthi239@gmail.com>
2021-07-21 11:32:15 +0530

    Moved files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 # from chatterbot import ChatBot
 # from chatterbot.trainers import ChatterBotCorpusTrainer
-import bot
+from functions import bot
 
 app = Flask(__name__)
 #create chatbot
